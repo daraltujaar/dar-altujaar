1953d8b817b9dc0472fc71905efc7002276c3fce
Dar Al-Tujar <admin@dar-altujaar.com>
2026-05-29 20:45:13 +0800

    Initial deploy - dar-altujaar.com

---
+++ sw.js
@@ -0,0 +1,60 @@
+// Service Worker for dar al-tujaar PWA
+const CACHE_VERSION = 'v4';
+const CACHE_NAME = `dartajir-${CACHE_VERSION}`;
+const STATIC_ASSETS = [
+  '/',
+  '/index.html',
+  '/404.html',
+  '/manifest.json',
+  '/images/hero-bg-pattern.jpg',
+  '/images/trader-cta-bg.jpg',
+  '/images/logo.png',
+  '/images/logo-header.png',
+  '/icons/icon-72x72.png',
+  '/icons/icon-96x96.png',
+  '/icons/icon-128x128.png',
+  '/icons/icon-144x144.png',
+  '/icons/icon-152x152.png',
+  '/icons/icon-192x192.png',
+  '/icons/icon-384x384.png',
+  '/icons/icon-512x512.png',
+];
+
+// Install - cache static assets
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(STATIC_ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate - clean old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch - network first, then cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        if (response && response.status === 200) {
+          const clone = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+        }
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
